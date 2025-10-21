d528fee0f44b92bb666c1e8533840dcce8f32b0d
Elian <elianacuna98@gmail.com>
2025-10-21 05:48:00 -0300

    local storage change

--- src/Middlewares/AuthMiddleware.jsx
+++ src/Middlewares/AuthMiddleware.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LOCALSTORAGE_KEYS from '../constants/localstorage'
+import LOCALSTORAGE_KEYS from '../constants/localStorageKeys'
 import { Navigate, Outlet } from 'react-router'
 
 const AuthMiddleware = () => {
